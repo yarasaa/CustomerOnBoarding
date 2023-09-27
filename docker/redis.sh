e429363b1789dfe7d265c0ed3c0dee2a15ada065
mdas <mdas@burgan.com.tr>
2023-09-27 00:52:22 -0700

    clean sln

--- docker/redis.sh
+++
@@ -1,5 +0,0 @@
-redis-server --daemonize yes && sleep 3
-redis-cli MSET config-amorphie-contract-db 'User ID=postgres;Password=postgres;Host=Localhost;Port=5432;Database=contract;'
-redis-cli save 
-redis-cli shutdown 
-redis-server
